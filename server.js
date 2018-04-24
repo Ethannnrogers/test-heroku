f2a1abb1b3d9c26e835cd0ccc083e0c5ed6ad37c
Ethan <erogers999@gmail.com>
2018-04-23 21:48:10 -0400

    newest update

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require('express');
+var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 5000;
 
+app.get("/", function(req, res) {
+  // res.send("Welcome to the Star Wars Page!")
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
 
 app.listen(PORT, function() {
